b72965e61a3d94ff377c94b742e93751c577e67e
Imad Selka <imadselkawi133@gmail.com>
2024-02-08 23:24:22 +0100

    event model

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -5,11 +5,12 @@ export interface IEvent extends Document {
   title: string;
   description?: string;
   location?: string;
-  createAt: string;
+  createdAt: Date;
   imageUrl: string;
   startDateTime: Date;
   endDateTime: Date;
-  price?: string;
+  price: string;
+  isSpecial: boolean;
   isFree: boolean;
   url?: string;
   category: { _id: string; name: string };
@@ -17,51 +18,18 @@ export interface IEvent extends Document {
 }
 
 const EventSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-  },
-  location: {
-    type: String,
-  },
-
-  createAt: {
-    type: String,
-    default: Date.now,
-  },
-  imageUrl: {
-    type: String,
-    required: true,
-  },
-  startDateTime: {
-    type: Date,
-    default: Date.now,
-  },
-  endDateTime: {
-    type: Date,
-    default: Date.now,
-  },
-  price: {
-    type: String,
-  },
-  isFree: {
-    type: Boolean,
-    default: false,
-  },
-  url: {
-    type: String,
-  },
-  category: {
-    type: Schema.Types.ObjectId,
-    ref: "Category",
-  },
-  organizer: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
+  title: { type: String, required: true },
+  description: { type: String },
+  location: { type: String },
+  createdAt: { type: Date, default: Date.now },
+  imageUrl: { type: String, required: true },
+  startDateTime: { type: Date, default: Date.now },
+  endDateTime: { type: Date, default: Date.now },
+  price: { type: String },
+  isFree: { type: Boolean, default: false },
+  url: { type: String },
+  category: { type: Schema.Types.ObjectId, ref: "Category" },
+  organizer: { type: Schema.Types.ObjectId, ref: "User" },
 });
 
 const Event = models.Event || model("Event", EventSchema);
